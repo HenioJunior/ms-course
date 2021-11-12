a9c57721b7abde1c6aefde830a03155a15cd53fe
Henio Junior <henioajunior@gmail.com>
2021-11-12 14:31:06 -0300

    RestTemplate

--- hr-payroll/src/main/java/com/heniojunior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/heniojunior/hrpayroll/services/PaymentService.java
@@ -1,14 +1,33 @@
 package com.heniojunior.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.heniojunior.hrpayroll.entities.Payment;
+import com.heniojunior.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
 	
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	
 	public Payment getPayment(long workerId, int days) {
-		return new Payment("Bob", 200.0, days);
+		
+		Map<String, String> uriVariable = new HashMap<>();
+		uriVariable.put("id", ""+workerId);		
+		
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariable);
+		
+		return new Payment(worker.getName(), worker.getDailyIncome() , days);
 	}
-
 }
