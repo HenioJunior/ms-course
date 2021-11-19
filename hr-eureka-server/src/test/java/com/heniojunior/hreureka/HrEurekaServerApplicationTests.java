eef0e6461b06cd33e6488f32f52fdc8f49b5dea5
Henio Junior <henioajunior@gmail.com>
2021-11-19 08:17:25 -0300

    Configuring Eureka Server

---
+++ hr-eureka-server/src/test/java/com/heniojunior/hreureka/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heniojunior.hreureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
