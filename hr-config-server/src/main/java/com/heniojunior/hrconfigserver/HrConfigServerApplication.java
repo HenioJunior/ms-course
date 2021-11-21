ae6d1986f584f9964a87bc7f373b4c38704e0fc3
Henio Junior <henioajunior@gmail.com>
2021-11-21 08:05:10 -0300

    Configuring hr-config-server project

---
+++ hr-config-server/src/main/java/com/heniojunior/hrconfigserver/HrConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.heniojunior.hrconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class HrConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrConfigServerApplication.class, args);
+	}
+
+}
