b5080150696a92dd20bbea35f4a3c2c15925910f
Henio Junior <henioajunior@gmail.com>
2021-11-24 09:09:32 -0300

    User, Role, many to many association

---
+++ hr-user/src/main/java/com/heniojunior/hruser/HrUserApplication.java
@@ -0,0 +1,15 @@
+package com.heniojunior.hruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class HrUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrUserApplication.class, args);
+	}
+
+}
