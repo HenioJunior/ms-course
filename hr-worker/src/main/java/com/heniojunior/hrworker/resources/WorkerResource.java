d5bd7343bdc471cc04cf813be7bf1aeb3d5b4d0c
Henio Junior <henioajunior@gmail.com>
2021-11-24 08:50:55 -0300

    Configuring hr-user project

--- hr-worker/src/main/java/com/heniojunior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/heniojunior/hrworker/resources/WorkerResource.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
