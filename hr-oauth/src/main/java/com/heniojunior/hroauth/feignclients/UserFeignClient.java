b87a07a1b0e5525d4d8592bde961dc3782f6b135
Henio Junior <henioajunior@gmail.com>
2021-11-25 14:58:04 -0300

    UserFeignClient

---
+++ hr-oauth/src/main/java/com/heniojunior/hroauth/feignclients/UserFeignClient.java
@@ -0,0 +1,19 @@
+package com.heniojunior.hroauth.feignclients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.heniojunior.hroauth.entities.User;
+
+
+@Component
+@FeignClient(name = "hr-user", path = "/users")
+public interface UserFeignClient {
+	
+	@GetMapping(value = "/search")
+	ResponseEntity<User> findByEmail(@RequestParam String email);
+
+}
