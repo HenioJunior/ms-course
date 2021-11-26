53789ebc485a4e46f362cc071be8e6b1b99b5449
HenioJunior <henioajunior@gmail.com>
2021-11-26 17:34:11 -0300

    Login e token JWT PARTE 2/2

--- hr-oauth/src/main/java/com/heniojunior/hroauth/entities/User.java
+++ hr-oauth/src/main/java/com/heniojunior/hroauth/entities/User.java
@@ -2,11 +2,17 @@ package com.heniojunior.hroauth.entities;
 
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 
-public class User implements Serializable {
+public class User implements UserDetails, Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
@@ -42,9 +48,57 @@ public class User implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+		
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 	public Set<Role> getRoles() {
 		return roles;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles.stream().map(x -> new SimpleGrantedAuthority(x.getRoleName()))
+				.collect(Collectors.toList());
+	}
+	
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
 		
 }
--- hr-oauth/src/main/java/com/heniojunior/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/heniojunior/hroauth/services/UserService.java
@@ -3,28 +3,41 @@ package com.heniojunior.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.heniojunior.hroauth.entities.User;
 import com.heniojunior.hroauth.feignclients.UserFeignClient;
 
 @Service
-public class UserService {
-	
+public class UserService implements UserDetailsService {
+
 	Logger logger = LoggerFactory.getLogger(UserService.class);
-	
+
 	@Autowired
 	private UserFeignClient userFeignClient;
-	
+
 	public User findByEmail(String email) {
-		
 		User user = userFeignClient.findByEmail(email).getBody();
-		if(user == null) {
+		if (user == null) {
 			logger.error("Email not found: " + email);
 			throw new IllegalArgumentException("Email not found");
 		}
 		logger.info("Email found: " + email);
-		return user;		
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(username).getBody();
+		if (user == null) {
+			logger.error("Email not found: " + username);
+			throw new IllegalArgumentException("Email not found");
+		}
+		logger.info("Email found: " + username);
+		return user;
 	}
 
 }
